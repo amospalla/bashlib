5ed702687402c3df40b0c28826458595a531507c
Jordi Marqués <jordi@amospalla.es>
2023-02-27 18:14:47 +0100

    style: fix blanks

--- examples/sample1/sample1.main.sh
+++ examples/sample1/sample1.main.sh
@@ -16,16 +16,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 main() {
-    __bl_log debug debug
-    __bl_log info info
-    __bl_log warning warning
-    __bl_log error error
-    __bl_log critical critical
-    __bl_echo_color green "sleep for 0.5 seconds"
+	__bl_log debug debug
+	__bl_log info info
+	__bl_log warning warning
+	__bl_log error error
+	__bl_log critical critical
+	__bl_echo_color green "sleep for 0.5 seconds"
 	__bl_sleep 0.5
-    false
-    true
-    echo end
+	false
+	true
+	echo end
 }
 
 # vim: set ft=sh:
